e30b6357bf190211393465063fb51363c8153954
Aman Sharma <amansharma7235@gmail.com>
2024-03-22 13:44:35 +0530

    Fifth Commit

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner'
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
   static defaultProps = {
@@ -21,70 +22,80 @@ export class News extends Component {
     this.state = {
       articles: [],
       loading: false,
-      page: 1
+      page: 1,
+      totalResults: 0
     }
   }
 
-  async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0a9ef1f3dd6e43af82b4622b475f31fa&page=1&pageSize=${this.props.pageSize}`;
-    this.setState({loading: true});
+  async updateNews() {
+    this.props.setProgress(15);
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
+    this.props.setProgress(50);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({ 
-      articles: parsedData.articles, 
+    this.setState({
+      articles: parsedData.articles,
       totalResults: parsedData.totalResults,
       loading: false
-     });
+    });
+    this.props.setProgress(100);
   }
 
-  handlePrevClick = async () => {
-    console.log("prev");
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0a9ef1f3dd6e43af82b4622b475f31fa&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-    this.setState({loading: true});
+  async componentDidMount() {
+    this.updateNews();
+  }
+
+  // handlePrevClick = async () => {
+  //   this.setState({page : this.state.page - 1});
+  //   this.updateNews();
+  // }
+
+  // handleNextClick = async () => {
+  //   this.setState({page : this.state.page + 1});
+  //   this.updateNews();
+  // }
+  fetchMoreData = async () => {
+    this.setState({page: this.state.page + 1})
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({ 
-      articles: parsedData.articles,
-      page: this.state.page - 1,
-      loading: false
-    })
-  }
-
-  handleNextClick = async () => {
-    console.log("Next");
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0a9ef1f3dd6e43af82b4622b475f31fa&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-      this.setState({loading: true})
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      console.log(parsedData);
-      this.setState({ 
-        articles: parsedData.articles,
-        page: this.state.page + 1,
-        loading: false
-      })
-  }
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults
+    });
+  };
   render() {
     return (
-      <div className='container my-3'>
+      <>
 
-        <h1 className='text-center' style={{margin:"20px"}}>NewsQuest - Top Headlines</h1>
-        {this.state.loading && <Spinner/>}
-        <div className='row'>
-          {/* map() method: The map() method is the most commonly used function to iterate over an array of data in JSX. 
+        <h1 className='text-center' style={{ margin: "20px" }}>NewsQuest - Top Headlines</h1>
+        {this.state.loading && <Spinner />}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length < this.state.totalResults}
+          loader={!this.state.loading && <Spinner />}
+        >
+          <div className='container my-3'>
+            <div className='row'>
+              {/* map() method: The map() method is the most commonly used function to iterate over an array of data in JSX. 
           You can attach the map() method to the array and use a callback function that gets called for each iteration. */}
-          {!this.state.loading && this.state.articles.map((element) => {
-            return <div className='col-md-3' key={element.url}>
-              <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} />
+              {this.state.articles.map((element) => {
+                return <div className='col-md-3' key={element.url}>
+                  <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} />
+                </div>
+              })}
             </div>
-          })}
-        </div>
-        {!this.state.loading && <div className='container d-flex justify-content-between'>
+          </div>
+        </InfiniteScroll>
+        {/* {this.state.loading && <div className='container d-flex justify-content-between'>
           <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
-          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
-        </div>}
-      </div>
+          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div>} */}
+      </>
     )
   }
 }
